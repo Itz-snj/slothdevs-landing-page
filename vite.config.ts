c368ef90e96f9cfa85123b9ebe1d2301230f3c0d
itz-snj <office.snj.2005@gmail.com>
2026-04-16 03:17:28 +0530

    tailwind config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tsConfigPaths(), tanstackStart(), nitro({ preset: "vercel" }), viteReact()],
+  plugins: [
+    tsConfigPaths(),
+    tailwindcss(),
+    tanstackStart(),
+    nitro({ preset: "vercel" }),
+    viteReact(),
+  ],
 });
